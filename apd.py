06a28c6079ed8cda1ac9f03d413a0adc8fdfbc5a
Dust1920 <a218208885@gmail.com>
2024-09-06 14:59:29 -0700

    modif

--- apd.py
+++ apd.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-
+from scipy.interpolate import CubicSpline
 # Numero de Etapas
 steps = 20
 
@@ -31,5 +31,17 @@ def last_pay(x):
 npay = np.vectorize(last_pay)
 
 jfunctions.loc[steps - 1] = npay(states)
+print(jfunctions.shape)
 
+for k in jfunctions.index:
+    print(k)
+    for x_n in states:
+        Ax = np.linspace(0.00001, x_n, 200)
+        ck = (beta ** k) * Ax ** (1 - gamma)
+        fx = (1 + ir) * (x_n - Ax)
+        interf = CubicSpline(states, jfunctions.loc[int(k)]) 
+        fadjust = [interf(x) for x in fx]
+        jfunctions.loc[int(k - 1), x_n] = np.max(ck + np.array(fadjust))
+    if k == 1:
+        break
 print(jfunctions)
