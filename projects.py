6886481cc0327ae1a9f083ee892e03ab783fbda1
Dust1920 <a218208885@gmail.com>
2024-10-14 15:54:45 -0700

    projects

---
+++ projects.py
@@ -0,0 +1,11 @@
+ACTUAL_PROJECT = "David"
+
+if ACTUAL_PROJECT == "David":
+    from DavidProject import main
+if ACTUAL_PROJECT == "Gabriel":
+    from GabrielProject import main
+if ACTUAL_PROJECT == "Italo":
+    from GabrielProject import main
+if ACTUAL_PROJECT == "Irasema":
+    from GabrielProject import main
+    
